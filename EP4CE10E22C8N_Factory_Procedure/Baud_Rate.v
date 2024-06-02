016d7edc0c3f85b012dd1c993a1fb4fecdb90064
Code-Newborn <12537091X@qq.com>
2024-06-02 10:24:54 +0800

    [comment] ...

--- EP4CE10E22C8N_Factory_Procedure/Baud_Rate.v
+++ EP4CE10E22C8N_Factory_Procedure/Baud_Rate.v
@@ -5,41 +5,35 @@
 // 开始日期: 2018-08-24
 // 完成日期: 2018-08-24
 // 修改日期:
-// 版    本: V1.0: 
-// 调试工具: 
+// 版    本: V1.0:
+// 调试工具:
 // 说    明:将50MHz时钟分成115200波特率
-//     
+//
 //--------------------------------------------------------------
-module Baud_Rate(
-	input wire clk_50m,		//时钟
-	output wire rxclk_en,	//波特率
-	output wire txclk_en
+module Baud_Rate (
+    input  wire clk_50m,   //时钟
+    output wire rxclk_en,  //波特率
+    output wire txclk_en
 );
 
-parameter RX_ACC_MAX = 50000000 / (115200 * 16);
-parameter TX_ACC_MAX = 50000000 / (115200 * 16) *16;
-parameter RX_ACC_WIDTH = $clog2(RX_ACC_MAX);
-parameter TX_ACC_WIDTH = $clog2(TX_ACC_MAX);
-reg [RX_ACC_WIDTH - 1:0] rx_acc = 1;
-reg [TX_ACC_WIDTH - 1:0] tx_acc = 1;
+    parameter RX_ACC_MAX = 50000000 / (115200 * 16);
+    parameter TX_ACC_MAX = 50000000 / (115200 * 16) * 16;
+    parameter RX_ACC_WIDTH = $clog2(RX_ACC_MAX);
+    parameter TX_ACC_WIDTH = $clog2(TX_ACC_MAX);
+    reg [RX_ACC_WIDTH - 1:0] rx_acc = 1;
+    reg [TX_ACC_WIDTH - 1:0] tx_acc = 1;
 
-assign rxclk_en = (rx_acc == 5'd1);
-assign txclk_en = (tx_acc == 9'd1);
+    assign rxclk_en = (rx_acc == 5'd1);
+    assign txclk_en = (tx_acc == 9'd1);
 
-always @(posedge clk_50m) 
-begin
-	if(rx_acc == RX_ACC_MAX[RX_ACC_WIDTH - 1:0])
-		rx_acc <= 1;
-	else
-		rx_acc <= rx_acc + 5'b1;
-end
+    always @(posedge clk_50m) begin
+        if (rx_acc == RX_ACC_MAX[RX_ACC_WIDTH-1:0]) rx_acc <= 1;
+        else rx_acc <= rx_acc + 5'b1;
+    end
 
-always @(posedge clk_50m) 
-begin
-	if(tx_acc == TX_ACC_MAX[TX_ACC_WIDTH - 1:0])
-		tx_acc <= 1;
-	else
-		tx_acc <= tx_acc + 9'b1;
-end
+    always @(posedge clk_50m) begin
+        if (tx_acc == TX_ACC_MAX[TX_ACC_WIDTH-1:0]) tx_acc <= 1;
+        else tx_acc <= tx_acc + 9'b1;
+    end
 
-endmodule 
+endmodule
--- EP4CE10E22C8N_Factory_Procedure/Factory_Procedure.v
+++ EP4CE10E22C8N_Factory_Procedure/Factory_Procedure.v
@@ -5,49 +5,49 @@
 // 开始日期: 2020-06-05
 // 完成日期: 2020-06-05
 // 修改日期:
-// 版    本: V1.0: 
-// 调试工具: 
+// 版    本: V1.0:
+// 调试工具:
 // 说    明:
-//     
+//
 //--------------------------------------------------------------
-module Factory_Procedure(
-	input clk,
-	input rst_n,
-	input RX,
-	output TX,
-	input K1,
-	input K2,
-	input K3,
-	input K4,
-	output LED_R,
-	output LED_G,
-	output LED_B
+module Factory_Procedure (
+    input  clk,
+    input  rst_n,
+    input  RX,
+    output TX,
+    input  K1,
+    input  K2,
+    input  K3,
+    input  K4,
+    output LED_R,
+    output LED_G,
+    output LED_B
 );
 
-// 串口通信
-UART_DATA UART_DATA_TEXT(
-	.din(),
-	.wr_en(1'b0),
-	.clk_50m(clk),
-	.tx(TX),
-	.tx_busy(),
-	.rx(RX),
-	.rdy(),
-	.rdy_clr(),
-	.dout()
-);
+    // 串口通信模块
+    UART_DATA UART_DATA_TEXT (
+        .din    (),
+        .wr_en  (1'b0),
+        .clk_50m(clk),
+        .tx     (TX),
+        .tx_busy(),
+        .rx     (RX),
+        .rdy    (),
+        .rdy_clr(),
+        .dout   ()
+    );
 
-// 按键测试及三色灯测试
-KEY U_KEY(
-	.clk(clk),
-	.rst_n(rst_n),
-	.k1_in(K1),
-	.k2_in(K2),
-	.k3_in(K3),
-	.k4_in(K4),
-	.LED_R(LED_R),
-	.LED_G(LED_G),
-	.LED_B(LED_B)
-);
+    // 按键测试及三色灯测试模块
+    KEY U_KEY (
+        .clk  (clk),
+        .rst_n(rst_n),
+        .k1_in(K1),
+        .k2_in(K2),
+        .k3_in(K3),
+        .k4_in(K4),
+        .LED_R(LED_R),
+        .LED_G(LED_G),
+        .LED_B(LED_B)
+    );
 
-endmodule 
+endmodule
